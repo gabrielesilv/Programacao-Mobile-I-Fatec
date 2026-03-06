564050f167fc8b717dd8dbfb39dd778d2679cb95
Tainá Kasai <122821654+gabrielesilv@users.noreply.github.com>
2026-03-06 10:38:39 -0300

    feat: Adiciona cálculo imc

--- ProjetoMobile/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ ProjetoMobile/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,9 +14,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val editTextAltura = findViewById<EditText>(R.id.editAltura)
+        val editTextPeso = findViewById<EditText>(R.id.editTextPeso)
+        val resultado = findViewById<TextView>(R.id.textViewResultado)
+        val buttoncalcular = findViewById<TextView>(R.id.buttoncalcular)
 
 
+        buttoncalcular.setOnClickListener {
+            val peso = editTextPeso.text.toString().toDoubleOrNull()
+            val altura = editTextAltura.text.toString().toDoubleOrNull()
 
+
+            if (altura == null || peso == null){
+                resultado.text = "Preencha os campos corretamente"
+                return@setOnClickListener
+            }
+
+
+            val imc = peso / (altura * altura)
+
+
+//            resultado.text = String.format("%.2f",imc)
+            resultado.text = "Seu IMC é: "+imc.toString()
+        }
 
     }
 }
